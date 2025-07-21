c297251e0585e4ed87ab46c6c5927201eabc47cd
Rayze421 <michaelsschmuck@gmail.com>
2025-07-21 14:27:41 -0400

    redid some wording?

--- voidstranger/LocationGroups.py
+++ voidstranger/LocationGroups.py
@@ -1,153 +1,153 @@
-from .Constants.LocationNames import LocationName
+from .Locations import LocationNames
 
 
 vs_location_groups = {
     "Add's Domain Chests": {
-        LocationName.b003_chest,
-        LocationName.b004_chest,
-        LocationName.b005_chest,
-        LocationName.b006_chest,
-        LocationName.b009_chest,
-        LocationName.b010_chest,
-        LocationName.bo12_chest,
-        LocationName.b012_chest,
-        LocationName.b014_chest,
-        LocationName.b018_chest,
-        LocationName.b024_chest,
-        LocationName.b025_chest,
-        LocationName.b026_chest
+        LocationNames.b003_chest,
+        LocationNames.b004_chest,
+        LocationNames.b005_chest,
+        LocationNames.b006_chest,
+        LocationNames.b009_chest,
+        LocationNames.b010_chest,
+        LocationNames.bo12_chest,
+        LocationNames.b012_chest,
+        LocationNames.b014_chest,
+        LocationNames.b018_chest,
+        LocationNames.b024_chest,
+        LocationNames.b025_chest,
+        LocationNames.b026_chest
     },
 
     "Eus' Domain Chests": {
-        LocationName.b032_chest,
-        LocationName.b033_chest,
-        LocationName.b034_chest,
-        LocationName.b035_chest,
-        LocationName.b036_chest,
-        LocationName.b037_chest,
-        LocationName.b040_chest,
-        LocationName.b041_chest,
-        LocationName.b043_chest,
-        LocationName.b048_chest,
-        LocationName.b050_chest
+        LocationNames.b032_chest,
+        LocationNames.b033_chest,
+        LocationNames.b034_chest,
+        LocationNames.b035_chest,
+        LocationNames.b036_chest,
+        LocationNames.b037_chest,
+        LocationNames.b040_chest,
+        LocationNames.b041_chest,
+        LocationNames.b043_chest,
+        LocationNames.b048_chest,
+        LocationNames.b050_chest
     },
 
     "Bee's Domain Chests": {
-        LocationName.b060_chest,
-        LocationName.b064_chest,
-        LocationName.b065_chest,
-        LocationName.b069_chest,
-        LocationName.b074_chest,
-        LocationName.b076_chest,
-        LocationName.b077_chest,
-        LocationName.b078_chest,
-        LocationName.b080_chest,
-        LocationName.b081_chest
+        LocationNames.b060_chest,
+        LocationNames.b064_chest,
+        LocationNames.b065_chest,
+        LocationNames.b069_chest,
+        LocationNames.b074_chest,
+        LocationNames.b076_chest,
+        LocationNames.b077_chest,
+        LocationNames.b078_chest,
+        LocationNames.b080_chest,
+        LocationNames.b081_chest
     },
 
     "Mon's Domain Chests": {
-        LocationName.b088_chest,
-        LocationName.b091_chest,
-        LocationName.b094_chest
+        LocationNames.b088_chest,
+        LocationNames.b091_chest,
+        LocationNames.b094_chest
     },
 
     "Tan's Domain Chests": {
-        LocationName.b115_chest,
-        LocationName.b116_chest,
-        LocationName.b118_chest,
-        LocationName.b122_chest,
-        LocationName.b123_chest,
-        LocationName.b127_chest,
-        LocationName.b133_chest,
-        LocationName.b135_chest
+        LocationNames.b115_chest,
+        LocationNames.b116_chest,
+        LocationNames.b118_chest,
+        LocationNames.b122_chest,
+        LocationNames.b123_chest,
+        LocationNames.b127_chest,
+        LocationNames.b133_chest,
+        LocationNames.b135_chest
     },
 
     "Gor's Domain Chests": {
-        LocationName.b144_chest,
-        LocationName.b145_chest,
-        LocationName.b151_chest,
-        LocationName.b159_chest,
-        LocationName.b160_chest1,
-        LocationName.b160_chest2,
-        LocationName.b166_chest
+        LocationNames.b144_chest,
+        LocationNames.b145_chest,
+        LocationNames.b151_chest,
+        LocationNames.b159_chest,
+        LocationNames.b160_chest1,
+        LocationNames.b160_chest2,
+        LocationNames.b166_chest
     },
 
     "Lev's Domain Chests": {
-        LocationName.b171_chest,
-        LocationName.b173_chest,
-        LocationName.b176_chest,
-        LocationName.b177_chest,
-        LocationName.b178_chest1,
-        LocationName.b178_chest2,
-        LocationName.b179_chest,
-        LocationName.b180_chest,
-        LocationName.b189_chest,
-        LocationName.b191_chest,
-        LocationName.b195_chest1,
-        LocationName.b195_chest2,
-        LocationName.b195_chest3,
-        LocationName.b195_chest4,
+        LocationNames.b171_chest,
+        LocationNames.b173_chest,
+        LocationNames.b176_chest,
+        LocationNames.b177_chest,
+        LocationNames.b178_chest1,
+        LocationNames.b178_chest2,
+        LocationNames.b179_chest,
+        LocationNames.b180_chest,
+        LocationNames.b189_chest,
+        LocationNames.b191_chest,
+        LocationNames.b195_chest1,
+        LocationNames.b195_chest2,
+        LocationNames.b195_chest3,
+        LocationNames.b195_chest4,
     },
 
     "Cif's Domain Chests": {
-        LocationName.b200_chest,
-        LocationName.b209_chest,
-        LocationName.b0210_chest
+        LocationNames.b200_chest,
+        LocationNames.b209_chest,
+        LocationNames.b0210_chest
     },
 
     "Idol Checks": {
-        LocationName.statue_lover,
-        LocationName.statue_smiler,
-        LocationName.statue_killer
+        LocationNames.statue_lover,
+        LocationNames.statue_smiler,
+        LocationNames.statue_killer
     },
 
     "Mural Checks": {
-        LocationName.mural_add,
-        LocationName.mural_eus,
-        LocationName.mural_bee,
-        LocationName.mural_mon,
-        LocationName.mural_tan,
-        LocationName.mural_gor,
-        LocationName.mural_lev,
-        LocationName.mural_cif,
-        LocationName.mural_dis
+        LocationNames.mural_add,
+        LocationNames.mural_eus,
+        LocationNames.mural_bee,
+        LocationNames.mural_mon,
+        LocationNames.mural_tan,
+        LocationNames.mural_gor,
+        LocationNames.mural_lev,
+        LocationNames.mural_cif,
+        LocationNames.mural_dis
     },
 
     "Burden Chests": {
-        LocationName.burden_chest1,
-        LocationName.burden_chest2,
-        LocationName.burden_chest3
+        LocationNames.burden_chest1,
+        LocationNames.burden_chest2,
+        LocationNames.burden_chest3
     },
 
     "Mon's Funhouse": {
-        LocationName.m14_chest1,
-        LocationName.m14_chest2,
-        LocationName.m14_chest3,
-        LocationName.m15_chest1,
-        LocationName.m15_chest2,
-        LocationName.m15_chest3,
-        LocationName.m15_chest4,
-        LocationName.m15_chest5,
-        LocationName.m15_chest6,
-        LocationName.m15_chest7,
-        LocationName.m15_chest8,
-        LocationName.m15_chest9,
-        LocationName.m15_chest10,
-        LocationName.m15_chest11,
-        LocationName.m15_chest12
+        LocationNames.m14_chest1,
+        LocationNames.m14_chest2,
+        LocationNames.m14_chest3,
+        LocationNames.m15_chest1,
+        LocationNames.m15_chest2,
+        LocationNames.m15_chest3,
+        LocationNames.m15_chest4,
+        LocationNames.m15_chest5,
+        LocationNames.m15_chest6,
+        LocationNames.m15_chest7,
+        LocationNames.m15_chest8,
+        LocationNames.m15_chest9,
+        LocationNames.m15_chest10,
+        LocationNames.m15_chest11,
+        LocationNames.m15_chest12
     },
 
     "Shop Checks": {
-        LocationName.buy_shortcut1,
-        LocationName.buy_shortcut2,
-        LocationName.buy_shortcut3,
-        LocationName.buy_shortcut4,
-        LocationName.buy_shortcut5
+        LocationNames.buy_shortcut1,
+        LocationNames.buy_shortcut2,
+        LocationNames.buy_shortcut3,
+        LocationNames.buy_shortcut4,
+        LocationNames.buy_shortcut5
     },
 
     "Seal Checks": {
-        LocationName.lust_slain,
-        LocationName.sloth_slain
+        LocationNames.lust_slain,
+        LocationNames.sloth_slain
     }
 
 
