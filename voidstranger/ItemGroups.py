903ef20e5d1ad948deb7897891284bc4d63e56cf
RoobyRoo <thegreenrobby@gmail.com>
2026-02-19 14:14:35 -0700

    Add itemgroup aliases

---
+++ voidstranger/ItemGroups.py
@@ -0,0 +1,24 @@
+from .Constants import ItemNames
+
+vs_item_groups = {
+    "Void Memory": {ItemNames.void_memory},
+    "Void Sword": {ItemNames.void_sword},
+    "Void Wings": {ItemNames.void_wings},
+    "Memory": {ItemNames.void_memory},
+    "Sword": {ItemNames.void_sword},
+    "Wings": {ItemNames.void_wings},
+    "Cube": {ItemNames.void_memory},
+    "Endless Void Rod": {ItemNames.endless_void_rod},
+    "Void Rod": {ItemNames.endless_void_rod},
+    "Interface": {ItemNames.interface_manip},
+    "Add's Brand": {ItemNames.brand_add},
+    "Eus's Brand": {ItemNames.brand_eus},
+    "Bee's Brand": {ItemNames.brand_bee},
+    "Mon's Brand": {ItemNames.brand_mon},
+    "Tan's Brand": {ItemNames.brand_tan},
+    "Gor's Brand": {ItemNames.brand_gor},
+    "Lev's Brand": {ItemNames.brand_lev},
+    "Cif's Brand": {ItemNames.brand_cif},
+    "DIS's Brand": {ItemNames.brand_dis},
+    "Progressive Brand": {ItemNames.brand_prog},
+    }
